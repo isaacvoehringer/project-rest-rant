41664b81488569999905125eb6d2bb24f7cdb928
isaac <voehringer.isaac@gmail.com>
2022-10-03 11:49:59 -0500

    sticky footer

--- views/default.jsx
+++ views/default.jsx
@@ -1,5 +1,6 @@
 const React = require('react')
-
+const today = new Date()
+const year = today.getFullYear()
 function Def (html) {
     return (
         <html>
@@ -8,10 +9,14 @@ function Def (html) {
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous"/>
 
                 <link rel="stylesheet" href="/css/style.css"/>
+                <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css"
+    />            
             </head>
             <body>
-                <nav className="navbar navbar-expand-lg">
-                    <ul>
+                <nav className="navbar bg-dark fixed-top flex-shrink-0">
+                    <ul className='container text-center'>
                         <li>
                             <a  href='/'>Home</a>
                         </li>
@@ -24,7 +29,10 @@ function Def (html) {
                     </ul>
                 </nav>
                 {html.children}
-
+        <footer className='fixed-bottom flex-shrink-0 py-2 bg-dark text-white'>
+            <div className='container text-center'>App by: Isaac V</div>
+            <div> &copy;IKE 2022-{year}</div>
+        </footer>
                 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
                 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -23,11 +23,12 @@ function show (data) {
                     no comments yet
                   <div>  
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
-                 Edit
+            <i className="bi bi-pencil"></i>Edit
             </a>  
+           
             <form method='POST' action={`/places/${data.id}?_method=DELETE`}> 
                 <button type="submit" className="btn btn-danger">
-                Delete
+                <i class="bi bi-trash"></i>Delete
                 </button>
             </form>     
 
