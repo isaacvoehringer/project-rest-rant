0c13228a6441a4f3e0fdfdab819e65a5665002b8
isaac <voehringer.isaac@gmail.com>
2022-10-09 17:05:59 -0500

    added validations

--- models/places.js
+++ models/places.js
@@ -2,11 +2,15 @@ const mongoose = require('mongoose')
 
 const placeSchema = new mongoose.Schema({
   name: {type: String, required: true },
-  pic: {type: String, },
+  pic: {type: String, default: 'http://placepuppy.com/350/350'},
   cuisines: {type: String, required: true  },
   city: {type: String, default: 'Nowhere' },
   state: {type: String, default: 'USA'},
-  founded: Number
+  founded: {
+    type: Number,
+    min: [1673, 'Surely not that old!?!?'],
+    max: [new Date().getFullYear(), 'Hey, this is in the future!']
+  }
 })
 
 placeSchema.methods.showEstablished = function() {
