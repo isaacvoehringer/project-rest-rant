fa817668f535d33525ea262a34fd279944f8e29c
isaac <voehringer.isaac@gmail.com>
2022-10-15 16:59:30 -0500

    comments

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,25 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+    let comments = (
+        <h3 className='inactive'>
+            No comments yet!
+        </h3>
+    )
+    if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+            return (
+                <div className='border'>
+                    <h2 className='rant'> {c.rant ? 'Rant! `:anguished:`' : 'Rave! `:smirk:`'}  </h2>
+                    <h4>{c.content}</h4>
+                    <h3>
+                        <strong>- {c.author}</strong>
+                    </h3>
+                    <h4>Rating: {c.stars}</h4>
+                </div>
+            )
+        })
+    }
     return (
         <Def>
             <main>
@@ -34,7 +53,7 @@ function show (data) {
             </div>
 </div>
 <h2>Comments</h2>
-                    <p>no comments yet</p> 
+                   {comments}
             </main>
         </Def>
     )
